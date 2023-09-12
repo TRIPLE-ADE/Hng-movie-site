fe24fc653963bb8ac273da3c2597164de1c2b6eb
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-09-12 15:15:17 +0100

    feat: Update header layout in components and Add Public assets

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import styles from '../style';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className={`bg-[#032541] ${styles.padding} ${styles.boxWidth}`}>
       <nav className={`${styles.headerFlex}`}>
         <Link href="/" className="text-xl font-bold text-white">
-          Movie Database
+            <Image src="/Logo.svg" width={100} height={50} alt="logo" />
         </Link>
         <ul className="flex space-x-4">
           
