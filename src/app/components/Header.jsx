4eb3ff39dd92fd109df91729d9342d40d5b9f3e7
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-09-15 19:55:24 +0100

    feat: Update header layout in component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,11 +8,12 @@ const Header = () => {
     <header className={`absolute ${styles.padding} ${styles.boxWidth}`}>
       <nav className={`${styles.headerFlex}`}>
         <Link href="/" className="text-xl font-bold text-white">
-            <Image src="/Logo.svg" width={80} height={80} alt="logo"  style={{ width: 'auto', height: 'auto' }} priority={true}/>
+            <Image src="/Logo.svg" width={80} height={80} alt="logo"  style={{ width: 'auto', height: 'auto' }} className='hidden sm:block' priority={true}/>
+            <Image src="/tv.svg" width={50} height={50} alt="logo"  style={{ width: 'auto', height: 'auto' }} className='sm:hidden' priority={true}/>
         </Link>
         <SearchInput />
         <div className='flex items-center justify-center gap-3'>
-            <p>Sign in</p>
+            <p className='hidden sm:block text-white font-bold'>Sign in</p>
             <button>
                 <Image src="/Menu.svg" width={36} height={36} alt="Menu" />
             </button>
